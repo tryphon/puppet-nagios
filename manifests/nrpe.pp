aa4a8fe06390c98039c9eb6968dea0116dedc0ac
Alban Peignier <alban@tryphon.eu>
2015-01-31 12:24:11 +0100

    Add nagios::nrpe::command define

--- manifests/nrpe.pp
+++ manifests/nrpe.pp
@@ -1,36 +1,43 @@
 class nagios::nrpe {
 
-  package { nagios-nrpe-server: }
+  package { 'nagios-nrpe-server': }
 
-  service { nagios-nrpe-server: 
+  service { 'nagios-nrpe-server':
     ensure => running,
-    require => [File["/etc/nagios/nrpe.d"],Package[nagios-nrpe-server]],
+    require => [File["/etc/nagios/nrpe.d"],Package['nagios-nrpe-server']],
     pattern => "/usr/sbin/nrpe"
   }
 
   file { "/etc/nagios/nrpe_local.cfg":
     content => template("nagios/nrpe_local.cfg"),
-    require => Package[nagios-nrpe-server],
-    notify => Service[nagios-nrpe-server]
+    require => Package['nagios-nrpe-server'],
+    notify => Service['nagios-nrpe-server']
   }
 
   file { "/etc/nagios/nrpe.d/":
     ensure => directory,
-    require => Package[nagios-nrpe-server]
+    require => Package['nagios-nrpe-server']
+  }
+
+  define command($definition) {
+    file { "/etc/nagios/nrpe.d/command_$name.cfg":
+      content => "command[$name]=$definition\n",
+      notify => Service['nagios-nrpe-server']
+    }
   }
 
   file { "/etc/nagios/nrpe.d/defaults.cfg":
     source => "puppet:///nagios/nrpe_defaults.cfg",
-    notify => Service[nagios-nrpe-server]
+    notify => Service['nagios-nrpe-server']
   }
 
-  package { nagios-plugins: }
+  package { 'nagios-plugins': }
 
   include nagios::nrpe::tiger
 }
 
 class nagios::nrpe::tiger {
   if $tiger_enabled {
-    tiger::ignore { nagios_nrpe: }
-  }  
+    tiger::ignore { 'nagios_nrpe': }
+  }
 }
