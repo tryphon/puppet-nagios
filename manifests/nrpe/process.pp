efaf92c3db4043454821dfbc258be1996903f57a
Florent Peyraud <florent.peyraud@af83.com>
2018-02-05 17:11:58 +0100

    fix syntax

--- manifests/nrpe/process.pp
+++ manifests/nrpe/process.pp
@@ -1,9 +1,9 @@
 class nagios::nrpe::process {
   nagios::nrpe::command { 'check_process_count_strict':
-    definition => '/usr/lib/nagios/plugins/check_procs -C '$ARG1$' -c $ARG2$:$ARG2$'
+    definition => '/usr/lib/nagios/plugins/check_procs -C $ARG1$ -c $ARG2$:$ARG2$'
   }
 
   nagios::nrpe::command { 'check_process_count':
-    definition => '/usr/lib/nagios/plugins/check_procs -C '$ARG1$' -c $ARG2$ -w $ARG3$'
+    definition => '/usr/lib/nagios/plugins/check_procs -C $ARG1$ -c $ARG2$ -w $ARG3$'
   }
 }
