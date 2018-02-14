3eda584d1a40069cb3c44e3a8af2ce23a5cf457e
Florent Peyraud <florent.peyraud@af83.com>
2018-02-14 22:22:52 +0100

    refactor puppet client check

--- manifests/puppet/client.pp
+++ manifests/puppet/client.pp
@@ -4,7 +4,15 @@ class nagios::puppet::client {
     target => '/usr/lib/nagios/plugins/utils.sh',
   }
 
-  nagios::plugin { 'check_file_not_exists': }
+  include nagios::plugins
+
+  file { "/usr/local/lib/nagios/plugins/check_file_not_exists":
+    source  => 'puppet:///modules/nagios/plugins/check_file_not_exists',
+    mode    => '0755',
+    require => File['/usr/local/lib/nagios/plugins'],
+  }
+ 
+  #nagios::plugin { 'check_file_not_exists': }
   nagios::nrpe::command { 'check_puppet_lock':
     definition => '/usr/local/lib/nagios/plugins/check_file_not_exists /var/lib/puppet/state/puppetdlock'
   }
